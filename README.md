e9cc6489c5f91bf4fd45df78014fd59876f06ee6
David Kim <daviddarden11@gmail.com>
2013-12-05 11:14:19 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Depot
+=====
+Shopping cart app from Agile web dev with Rails, 4th edition by Sam Ruby, Dave Thomas and DHH
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '4eb791d283be4ddabf2822773de4c7f2683bb6c69533c48fb6771f0afd8447d453d561a0abc89f6ad38fa681dab897de6ef96cbc28b362dae6ee9ab564dc907a'
