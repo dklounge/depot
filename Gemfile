8a31d336dd1e465e3b6d896e8f7464747ef8f676
David Kim <daviddarden11@gmail.com>
2013-12-26 16:01:00 -0800

    added paginate

--- Gemfile
+++ Gemfile
@@ -6,12 +6,9 @@ gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
-
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
-
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
+gem 'will_paginate', '~> 3.0'
 
 gem 'guard', :group => :development
 gem 'thin'
--- app/views/line_items/create.js
+++ app/views/line_items/create.js
@@ -7,3 +7,10 @@ $('form').on('submit', function (event) {
   // the following relates to view/line_items/_line_item.html.erb
   $("#current_item").effect("highlight", { color: "#88ff88" }, 1000);
 });
+
+// From Chapter 12 - to hide #notice after order
+// page.select('#notice').each { |notice| notice.hide }
+
+// page.replace_html('cart', render @cart)
+// page[:cart].visual_effect :blind_down if @cart.total_items == 1
+// page[:current_item].visual_effect :highlight, :startcolor => '#88ff88', :endcolor => "#114411"
